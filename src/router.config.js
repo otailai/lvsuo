797970a13d5f0cf449caf3ac314e72015248adc6
yhr1030507142 <1030507142@qq.com>
2019-04-02 17:25:25 +0800

    律所one

---
+++ src/router.config.js
@@ -0,0 +1,6 @@
+import index from './components/index'
+import login from './components/login'
+export default[
+    {path:'/login',components:login,name:login},
+    {path:'/index',components:index,name:index},
+]
